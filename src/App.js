1c90a20f1d59c86cf2489d7819c53910769fac93
Cleyton Caminha <cleytoncaminha01@gmail.com>
2022-02-23 16:47:00 -0300

    clean code 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react from "react";
+
 import { Menu } from "./components/header/index.js";
 import { GlobalStyle } from "./styles/global.js";
 import { About } from "./components/main/about/index.js";
